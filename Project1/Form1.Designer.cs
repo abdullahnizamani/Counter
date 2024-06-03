3fa6a993dcfa8044e24dd4747928ecee706009ba
Muhammad Abdullah <Muhammad Abdullah@DESKTOP-P469VTF>
2024-06-04 00:17:29 +0500

    Add project files.

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblCount = new Label();
+            btnCount = new Button();
+            SuspendLayout();
+            // 
+            // lblCount
+            // 
+            lblCount.AutoSize = true;
+            lblCount.Location = new Point(205, 52);
+            lblCount.Name = "lblCount";
+            lblCount.Size = new Size(0, 20);
+            lblCount.TabIndex = 0;
+            // 
+            // btnCount
+            // 
+            btnCount.Location = new Point(220, 187);
+            btnCount.Name = "btnCount";
+            btnCount.Size = new Size(206, 60);
+            btnCount.TabIndex = 1;
+            btnCount.Text = "Count";
+            btnCount.UseVisualStyleBackColor = true;
+            btnCount.Click += btnCount_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCount);
+            Controls.Add(lblCount);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblCount;
+        private Button btnCount;
+    }
+}
---
+++ Project1/Form1.cs
@@ -0,0 +1,17 @@
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a;
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            a+= 1;
+            lblCount.Text =Convert.ToString(a);
+        }
+    }
+}
